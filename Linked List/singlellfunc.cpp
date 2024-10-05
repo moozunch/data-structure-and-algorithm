d262488137bbba7b5ee06acc2dde35307cae0328
annisa :b <annisaputriaprilia12@gmail.com>
2024-10-05 17:21:07 +0700

    add, delete and change in desired position linked list

--- Linked List/singlellfunc.cpp
+++ Linked List/singlellfunc.cpp
@@ -39,15 +39,27 @@ void createLinkedlist(int jumlahnode) {
     cout << "\nAll nodes are created successfully!\n";
 }
 
+
+int count() {
+    Movie *current = head;
+    int count = 0;
+    //logika yang sama dengan printing dan tranversing linked list
+    while (current != nullptr) {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
+
 void print() {
     Movie *current = head;
-    int count = 1;
+    cout << "There are " << count() << " nodes in the linked list\n";
     
     while (current != nullptr) {
         cout << "Title " << count << ": " << current->title << "\n";
         cout << "Director " << count << ": " << current->director << "\n";
         cout << "Year " << count << ": " << current->year << "\n";
-        count++;
         current = current->next;
     }
 }
@@ -83,6 +95,31 @@ void addBack(){
         tail = newNode; //lastly jangan lupa ubah tail sekarang jadi newNode
 }
 
+void addPosition(int posisi) {
+    //Logika nya kita bakal tranversing dulu ke posisi sebelum posisi, untuk ganti next node nya jadi ke ke node baru kita.
+
+    Movie *current = head;
+    int i = 1;
+    while (i < posisi - 1) { //karena kita mau ke posisi sebelum posisi yang diinput
+        current = current->next; //setelah dapat current nya
+        i++;
+    }
+    Movie *newNode = new Movie; //buat node baru buat nyimpen data baru
+    cout << "Enter the title of the Movie: ";
+    cin.ignore();
+    getline(cin, newNode->title);
+
+    cout << "Enter the director of the Movie: ";
+    getline(cin, newNode->director);
+        
+    cout << "Enter the year of the Movie: ";
+    cin >> newNode->year; 
+    newNode->next = current->next; 
+    current->next = newNode; //next nya kita ke newNode
+    //Logika nya adalah kan yang current posisi yang setelah nya mau kita sisipi new data/node, nah otomatis si current ini pointer nya nunjuk ke newNode dan newNode pointer nya nunjuk ke next node yang sebelumnya di pegang current. Ingat kita main pointer jadi perubahan di current langsung ke node yang dia tunjuk.
+       
+}
+
 void deletFront(){
     Movie *del;
     del = head; //del mengcopy dan menunjuk ke head currently
@@ -111,6 +148,36 @@ void deleteBack(){
     delete del; //delete the last node
 }
 
+void deletePosition(int posisi) {
+    //Handling error position
+    if (posisi < 1) {
+        cout << "Invalid position!" << endl;
+        return;
+    } else if (posisi == 1) {
+        deletFront();
+        return;
+    } else if (posisi >= count()) {
+        cout << "Invalid position! (HINT: OUT OF RANGE)" << endl;
+        return;
+    } else if (posisi == count()) {
+        deleteBack();
+        return;
+    }
+
+    Movie *del, *current = head;
+    int i = 1; 
+    //logika nya sama kayak addPosition dan deleteBack, kita tranverse dulu ke posisi sebelum posisi yang diinput, lalu kita delete node yang diinput.
+    while (i < posisi - 1) {
+        current = current->next; //current naik terus ke current selanjutnya
+        i++;
+    }
+    del = current->next; //del nya di set ke node yang mau dihapus
+    current->next = del->next; //karena del mau dihapus, maka penunjuk pointer dari del kita pindahin dulu ke current
+    delete del; 
+}
+
+
+
 
 int main() {
 // Inisialisasi
@@ -135,6 +202,21 @@ int main() {
     cout << "\n\n\n";
     deleteBack();
     print();
+
+    cout << "\n\n\n";
+    int posisi;
+    cout << "Which position do you want to add the node? ";
+    cin >> posisi;
+    addPosition(posisi);
+    print();
+
+    cout << "\n\n\n";
+    cout << "Which position do you want to add the node? ";
+    cin >> posisi;
+    deletePosition(posisi);
+    print();
+
+    //bisa juga buat function buat changes, ya logika nya sama kek create newnode tapi kita ubah langsung di current itu setelah di tranverse di posisi yang diinginkan.
 }    
 
 
